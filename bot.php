b3ceaa8c58310fc2a377915bc6461c4ddc061560
alroysh <cicak.jatuh02@gmail.com>
2017-04-11 19:45:39 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -38,7 +38,7 @@
 	$ip = $_SERVER['REMOTE_ADDR'];
 	$browser= $_SERVER['HTTP_USER_AGENT'];
 	$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
-	$owner = array("owner","Owner");
+	$owner = "Owner";
 	//pesan bergambar
 if($message['type']=='text')
 	{
