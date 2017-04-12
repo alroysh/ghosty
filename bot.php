4ded52c3f9d05d487a0abae7a9b6d0d95bbb299b
alroysh <cicak.jatuh02@gmail.com>
2017-04-12 19:00:22 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -39,8 +39,7 @@
 	$browser= $_SERVER['HTTP_USER_AGENT'];
 	$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
 	$owner = "Owner";
-for ($x = 0; $x <= 10; $x++)
-if($message['type']=='text')
+
 	{
 		if($pesan_datang=='Halo')
 		{
